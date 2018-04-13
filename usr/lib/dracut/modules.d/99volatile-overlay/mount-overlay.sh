af97c9273ea2a2c449817fc41197dad6ba5037c7
Fabian Vogt <fvogt@suse.com>
2018-04-13 14:46:50 +0200

    Add new 99volatile-overlay dracut module
    This module mounts /etc and /var as an overlay with tmpfs as upper layer.
    It's unfortunately not possible to use fstab.sys for this, as workdir and
    upperdir need to be created underneath the same tmpfs mountpoint.
    
    This is intended as an alternative to the /var/lib/overlay use.

---
+++ usr/lib/dracut/modules.d/99volatile-overlay/mount-overlay.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
+type det_fs >/dev/null 2>&1 || . /lib/fs-lib.sh
+
+overlaydir="${NEWROOT}/tmp"
+
+mkdir -p "${overlaydir}"
+mount -t tmpfs tmpfs "${overlaydir}"
+mkdir ${overlaydir}/{etc,work-etc,var,work-var}
+
+mount -t overlay overlay "${NEWROOT}/etc -o upperdir=${overlaydir}/etc,workdir=${overlaydir}/work-etc,lowerdir=${NEWROOT}/etc
+mount -t overlay overlay "${NEWROOT}/var -o upperdir=${overlaydir}/var,workdir=${overlaydir}/work-var,lowerdir=${NEWROOT}/var
