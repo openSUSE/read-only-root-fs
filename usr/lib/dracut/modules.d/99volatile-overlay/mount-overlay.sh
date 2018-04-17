7f099188de3a839a1ab6bd019e4fe59d3d447534
Olaf Hering <olaf@aepfle.de>
2018-04-17 23:57:14 +0200

    proper shell escaping in mount-overlay.sh
    Fixes commit af97c9273ea2a2c449817fc41197dad6ba5037c7 ("Add new 99volatile-overlay dracut module")
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- usr/lib/dracut/modules.d/99volatile-overlay/mount-overlay.sh
+++ usr/lib/dracut/modules.d/99volatile-overlay/mount-overlay.sh
@@ -7,7 +7,7 @@ overlaydir="${NEWROOT}/tmp"
 
 mkdir -p "${overlaydir}"
 mount -t tmpfs tmpfs "${overlaydir}"
-mkdir ${overlaydir}/{etc,work-etc,var,work-var}
+mkdir "${overlaydir}"/{etc,work-etc,var,work-var}
 
-mount -t overlay overlay "${NEWROOT}/etc -o upperdir=${overlaydir}/etc,workdir=${overlaydir}/work-etc,lowerdir=${NEWROOT}/etc
-mount -t overlay overlay "${NEWROOT}/var -o upperdir=${overlaydir}/var,workdir=${overlaydir}/work-var,lowerdir=${NEWROOT}/var
+mount -t overlay overlay "${NEWROOT}/etc" -o "upperdir=${overlaydir}/etc,workdir=${overlaydir}/work-etc,lowerdir=${NEWROOT}/etc"
+mount -t overlay overlay "${NEWROOT}/var" -o "upperdir=${overlaydir}/var,workdir=${overlaydir}/work-var,lowerdir=${NEWROOT}/var"
